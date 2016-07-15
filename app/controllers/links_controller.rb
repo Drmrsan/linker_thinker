82f507d3ba4e2d1370714187a2d6f9b08964f318
drmrsan <drmrsan@gmail.com>
2016-07-15 13:18:29 +0000

    Initial commit

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,49 @@
+class LinksController < ApplicationController
+	before_action :find_link, only: [:show, :edit, :destroy]
+	before_filter :authenticate_user!, except: [:index, :show]
+	def index
+		@links = Link.all
+		@link  = Link.new
+	end
+
+	def new
+		@link = current_user.links.build
+	end
+
+	def create
+		@link = current_user.links.build (link_params)
+		# @link.user_id == current_user
+
+		if @link.save
+			redirect_to root_path, notice: "New link created"
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@link.destroy
+		redirect_to :back
+	end
+
+	def upvote
+		@link = Link.find(params[:id])
+		@link.upvote_by current_user
+		redirect_to :back
+	end
+
+	def downvote
+		@link = Link.find(params[:id])
+		@link.downvote_by current_user
+		redirect_to :back
+	end
+
+	private
+	def link_params
+		params.require(:link).permit(:title, :link, :description)
+	end
+
+	def find_link
+		@link = Link.find(params[:id])
+	end
+end
